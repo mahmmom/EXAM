c14ef3cbddb21cfeab726e34930e56b14b124e41
mahmmom <138707294+mahmmom@users.noreply.github.com>
2023-09-16 15:03:09 +0400

    Create ft_itoa.c

---
+++ ft_itoa.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+
+static int	num_digit(long num)
+{
+	int	digit;
+
+	digit = 0;
+	if (num == 0)
+		return (1);
+	if (num < 0)
+		digit++;
+	while (num != 0)
+	{
+		num = num / 10;
+		digit++;
+	}
+	return (cur);
+}
+
+char	*ft_itoa(int n)
+{
+	long	len;
+	long	nl;
+	char	*result;
+
+	len = num_digit(n);
+	nl = n;
+	if (n < 0)
+		nl *= -1;
+	result = malloc(sizeof(char) * (len + 1));
+	if (!result)
+		return (NULL);
+	result[len] = 0;
+	if (nl == 0)
+		result[0] = '0';
+	else
+	{
+		while (len--, nl != 0)
+		{
+			result[len] = (nl % 10) + '0';
+			nl = (nl - (nl % 10)) / 10;
+		}
+		if (n < 0)
+			result[len] = '-';
+	}
+	return (result);
+}
